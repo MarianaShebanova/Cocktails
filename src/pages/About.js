ad1f02bd03209dd4dbddd3e4362a30b7e9bd0691
MarianaShebanova <mariana.shebanova@gmail.com>
2021-09-27 10:23:30 -0700

    Initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum incidunt
+        impedit soluta veritatis at accusamus labore. Omnis cupiditate libero,
+        nesciunt accusamus quaerat quidem? Rerum aut quas eaque nisi laborum.
+        Voluptatibus veniam quasi atque unde ratione blanditiis, nemo eveniet
+        eaque! Perferendis.
+      </p>
+    </section>
+  );
+};
+
+export default About;
